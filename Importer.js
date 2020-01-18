ad9e6e694fa679e9489778376f3c2c2985dad118
Ken Whatmough <ken.whatmough@bluescape.com>
2020-01-18 14:01:39 -0500

    fix for ie and edge

--- Importer.js
+++ Importer.js
@@ -4,7 +4,7 @@ function Importer(moduleSpecifier) {
 
 Importer.prototype.then = function(onFulfilled, onRejected) {
   try {
-    import(this.moduleSpecifier).then(onFulfilled, onRejected);
+    new Function("return import('" + this.moduleSpecifier + "');")().then(onFulfilled, onRejected);
   } catch (err) {
     onRejected("Dynamic import failed: " + err.message);
   }
